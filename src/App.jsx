e5595120a9b3ec8b1309ac82ecf2cf9c7f03feb9
Lean <cxclx@outlook.com>
2023-06-09 00:26:21 +0200

    Rework

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import "./App.scss";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/header/Header";
+import './App.scss'
 
 function App() {
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Header />}></Route>
-      </Routes>
-    </Router>
-  );
+    <>
+      
+    </>
+  )
 }
 
-export default App;
+export default App
